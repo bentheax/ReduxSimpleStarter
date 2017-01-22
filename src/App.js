2ce624b7267211e6a417fb39fa66850e91358c6e
Ben Axnick <ben.axnick@flippa.com>
2017-01-22 06:56:56 +0000

    Initialize app with some youtube search state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: process.env.REACT_APP_YOUTUBE_API_KEY}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
   render() {
     return (
       <SearchBar />
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return (
-      <input />
+      <div>
+        <input
+          onChange={ (event) => this.setState({ term: event.target.value}) }
+          value={this.state.term}
+        />
+      </div>
     );
   }
 }
